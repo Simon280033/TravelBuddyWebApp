b85e834a0b5ddb7e92ca44d8ef04f86ef97175d4
Simon280033 <71387594+Simon280033@users.noreply.github.com>
2021-11-28 17:28:57 +0100

    Added basic infrastructure for editing tours
    Added basics

---
+++ AuthenticationTest/Data/Entities/Sight.cs
@@ -0,0 +1,15 @@
+using System.Drawing.Imaging;
+namespace AuthenticationTest.Data.Entities
+{
+    public class Sight
+    {
+        public int Id { get; set; }
+        public int TourId { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public double Latitude { get; set; }
+        public double Longitude { get; set; }
+        public int RadiusInMeters { get; set; }
+        public string image { get; set; }
+    }
+}
