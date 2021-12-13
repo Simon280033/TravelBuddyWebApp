03ccbf06f404cfe28b7703bb65aad24e30ebf4cb
Simon280033 <280033@viauc.dk>
2021-12-13 12:34:47 +0100

    Added DAO method for updating tour variants, including deletion

--- AuthenticationTest/Data/DAOs/Concrete/TourDAO.cs
+++ AuthenticationTest/Data/DAOs/Concrete/TourDAO.cs
@@ -193,7 +193,91 @@ namespace AuthenticationTest.Data
 
         public void updateTour(Tour tour)
         {
-            throw new System.NotImplementedException();
+           Console.WriteLine("Attempting to update tour...");
+            using (NpgsqlCommand command = new NpgsqlCommand())
+            {
+                // We get the current variants
+                List<string> variantCodes = new List<string>();
+                command.CommandText = "SELECT language_code FROM travelbuddy.tour_variants WHERE tour_id = @tourId;";
+                command.Parameters.AddWithValue("tourId", tour.Id);
+                command.Connection = conn;
+                conn.Open();
+                using (NpgsqlDataReader sdr = command.ExecuteReader())
+                {
+                    while (sdr.Read())
+                    {
+                        variantCodes.Add(sdr["language_code"].ToString());
+                    }
+                }
+                
+                // We update the tour itself
+                command.CommandText = "UPDATE travelbuddy.Tours SET tour_image = @imageBase64";
+                command.Connection = conn;
+                command.Parameters.AddWithValue("imageBase64", tour.ImageBase64);
+                command.Prepare();
+                command.ExecuteNonQuery();
+                command.Cancel();
+                Console.WriteLine("Updated tour! Attempting to update " + tour.Variants.Count + " variants...");
+                
+                // We make a list of the new variants
+                List<string> newVariants = new List<string>();
+                // We then update any variants
+                for (int i = 0; i < tour.Variants.Count; i++)
+                {
+                    // If it is not in the DB already, we create it
+                    if (!variantCodes.Contains(tour.Variants[i].Language.LanguageCode))
+                    {
+                        command.CommandText = "INSERT INTO travelbuddy.Tour_variants VALUES(@tourId, @languageCode, @tourName, @tourDescription);";
+                        command.Connection = conn;
+                        conn.Open();
+                        command.Parameters.AddWithValue("tourId", tour.Id);
+                        command.Parameters.AddWithValue("companyId", tour.CompanyId);
+                        command.Parameters.AddWithValue("languageCode", tour.Variants[i].Language.LanguageCode);
+                        command.Parameters.AddWithValue("tourName", tour.Variants[i].TourName);
+                        command.Parameters.AddWithValue("tourDescription", tour.Variants[i].TourDescription);
+                        command.Prepare();
+                        command.ExecuteNonQuery();
+                        command.Cancel();
+                        Console.WriteLine("Created variant for language '" + tour.Variants[i].Language.LanguageName + "'!");
+                    }
+                    else
+                    {
+                        // If it is already in there, we update it
+                        command.CommandText = "UPDATE travelbuddy.Tour_variants SET tour_name = @tourName, tour_description = @tourDescription WHERE tour_id = @tourId AND language_code = @languageCode;";
+                        command.Connection = conn;
+                        command.Parameters.AddWithValue("tourId", tour.Id);
+                        command.Parameters.AddWithValue("languageCode", tour.Variants[i].Language.LanguageCode);
+                        command.Parameters.AddWithValue("tourName", tour.Variants[i].TourName);
+                        command.Parameters.AddWithValue("tourDescription", tour.Variants[i].TourDescription);
+                        command.Prepare();
+                        command.ExecuteNonQuery();
+                        command.Cancel();
+                        Console.WriteLine("Updated variant for language '" + tour.Variants[i].Language.LanguageName + "'!");
+                    }
+                    // We add language to the new list
+                    newVariants.Add(tour.Variants[i].Language.LanguageCode);
+                }
+                // Now we check if any of the variants were deleted
+                // We go through the old variants, and see if they exist in the new
+                foreach (string code in variantCodes)
+                {
+                    if (!newVariants.Contains(code))
+                    {
+                        Console.WriteLine("Attempting to delete variant for language '" + code + "'...");
+                        // WE NEED TO ADD ON DELETE CASCADE
+                        command.CommandText = "DELETE travelbuddy.Tour_variants WHERE tour_id = @tourId AND language_code = @languageCode;";
+                        command.Connection = conn;
+                        command.Parameters.AddWithValue("tourId", tour.Id);
+                        command.Parameters.AddWithValue("languageCode", code);
+                        command.Prepare();
+                        command.ExecuteNonQuery();
+                        command.Cancel();
+                        Console.WriteLine("Succesfully deleted variant for language '" + code + "'!");
+                    }
+                }
+                conn.Close();
+            }
+            Console.WriteLine("Successfully updated tour!");        
         }
     }
 }
