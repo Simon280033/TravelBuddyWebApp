7cde4b083e1b91efeff22376b848933d237f6a53
Simon280033 <71387594+Simon280033@users.noreply.github.com>
2021-12-16 14:38:38 +0100

    Added languages

--- AuthenticationTest/Data/DAOs/Concrete/LanguageDAO.cs
+++ AuthenticationTest/Data/DAOs/Concrete/LanguageDAO.cs
@@ -1,7 +1,10 @@
+using System;
 using System.Collections.Generic;
 using System.Data;
+using System.IO;
 using AuthenticationTest.Data.Entities;
 using Npgsql;
+using Json.Net;
 
 namespace AuthenticationTest.Data
 {
@@ -45,6 +48,42 @@ namespace AuthenticationTest.Data
             {
                 conn.Open();
             }
+        }
+        
+        public void LoadJson()
+        {
+            using (StreamReader r = new StreamReader("langs.json"))
+            {
+                string json = r.ReadToEnd();
+                List<JSONClass> items = JsonNet.Deserialize<List<JSONClass>>(json);
+                
+                foreach (var item in items)
+                {
+                    Console.WriteLine("Code: " + item.code + ", name: " + item.name);
+                    SeedLanguage(item);
+                }
+            }
+        }
+        public class JSONClass
+        {
+            public string name { get; set; }      
+            public string code { get; set; }
+        }
+        
+        private void SeedLanguage(JSONClass item)
+        {
+            OpenConnIfClosed();
+            using (NpgsqlCommand command = new NpgsqlCommand())
+            {
+                command.CommandText =
+                    "INSERT INTO travelbuddy.Languages VALUES(@code, @name) ON CONFLICT DO NOTHING;";
+                command.Connection = conn;
+                command.Parameters.AddWithValue("code", item.code.ToUpper());
+                command.Parameters.AddWithValue("name", item.name);
+                command.Prepare();
+                command.ExecuteNonQuery();
+            }
+            conn.Close();
         }
     }
 }
